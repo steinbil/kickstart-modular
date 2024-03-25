79b73fa481c9f118e3ab5a913aea2d37836f575a
Steinar Refsdal <stonewebs@gmail.com>
2024-03-25 23:50:19 +0100

    Set: formatters for python, js

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -11,11 +11,11 @@ return {
         lua = { 'stylua' },
         c = { 'astyle' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
