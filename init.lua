4edef3e17e66339f9a963d0e32b5b41c5d067204
Steinar Refsdal <stonewebs@gmail.com>
2024-03-26 00:32:00 +0100

    Add commands.lua, require in init, add conform commands

--- init.lua
+++ init.lua
@@ -102,5 +102,8 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[ Extra User Commands ]]
+require 'commands'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
