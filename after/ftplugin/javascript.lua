b48e9083112eb8d163f959e24d56aba78fb338b6
Steinar Refsdal <stonewebs@gmail.com>
2024-03-25 22:16:53 +0100

    Add some plugs + ftplugin settings

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,8 @@
+-- These settings will effect any c. file
+
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
+vim.opt_local.textwidth = 79
+vim.opt_local.autoindent = true
+vim.opt_local.smarttab = true
---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- These settings will effect any .md file 
+
+-- Set conceallevel to 2 for markdown files to enable obsidian-plugin to do its thing...
+vim.opt_local.conceallevel = 2
