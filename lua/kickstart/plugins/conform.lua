7c2609263306594af18da7590e0b57c76081cf7b
Steinar Refsdal <stonewebs@gmail.com>
2024-03-25 23:41:54 +0100

    Try/fail to add recipe to conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'astyle' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
